ba7eb22197c0fecaa9324419bc825df31b6e7447
Alexandre32 <alexandre.baudesson.stella@gmail.com>
2016-03-07 09:36:19 +0100

    Commit initial de la frame et de la partie

---
+++ src/main/java/VgenAbau/Frame.java
@@ -0,0 +1,14 @@
+package VgenAbau;
+
+/**
+ * Created by 21102934 on 07/03/2016.
+ */
+public class Frame {
+
+    int lancer[];
+
+
+    public Frame() {
+        this.lancer = new int[2];
+    }
+}
---
+++ src/main/java/VgenAbau/Partie.java
@@ -0,0 +1,28 @@
+package VgenAbau;
+
+/**
+ * Created by 21102934 on 07/03/2016.
+ */
+public class Partie {
+
+    Frame lancers[];
+    Frame lancersComplementaires;
+
+    public Partie() {
+        lancers = new Frame[10];
+        lancersComplementaires = new Frame();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
